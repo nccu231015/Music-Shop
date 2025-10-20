aacb93833402a032206ccc84f1636b842ef1ab1a
nccu231015 <istudiodesign.tw@gmail.com>
2025-10-20 12:51:41 +0800

    Update: Change n8n webhook URL to istudion8nlab.org

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 環境配置檔案
 const CONFIG = {
     // n8n Webhook URL
-    N8N_WEBHOOK: 'https://n8n.n8-n.org/webhook/009e809a-43de-4b8f-bc5a-e49f33f7558c',
+    N8N_WEBHOOK: 'http://n8n.istudion8nlab.org/webhook/009e809a-43de-4b8f-bc5a-e49f33f7558c',
     
     // 圖片下載服務 URL
     // 生產環境 (Cloud Run)
